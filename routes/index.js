6ddaeda634dae1820832eacb32fc5d251745b236
longvo00221 <longvo010203@gmail.com>
2024-04-03 19:27:28 +0700

    final push

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import usersRoute from './users.route.js';
-// import postsRoute from './posts.route.js';
 const router = express.Router()
 
 router.use('/users',usersRoute)
-// router.use('/posts',postsRoute)
 
 
 export default router;
--- routes/posts.route.js
+++
@@ -1,7 +0,0 @@
-// import express from 'express';
-// import postsController from "../controller/post.controller.js";
-
-// const router = express.Router();
-// router.get('/:user', postsController.getPostsByUser);
-
-// export default router;
